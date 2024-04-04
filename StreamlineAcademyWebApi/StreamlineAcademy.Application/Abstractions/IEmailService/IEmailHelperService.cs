1b6306e484a850031670f684ad3b236bfe3dd325
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-04-04 15:31:07 +0530

    spaces trimmed

--- StreamlineAcademyWebApi/StreamlineAcademy.Application/Abstractions/IEmailService/IEmailHelperService.cs
+++ StreamlineAcademyWebApi/StreamlineAcademy.Application/Abstractions/IEmailService/IEmailHelperService.cs
@@ -8,10 +8,8 @@ namespace StreamlineAcademy.Application.Abstractions.IEmailService
 {
 	public interface IEmailHelperService
 	{
-
      	Task<bool> SendRegistrationEmail(string emailAddress, string name, string password);
 		Task<bool> SendResetPasswordEmail(string emailAddress, string confirmationCode);
-
 
 	}
 }
