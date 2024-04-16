69057f78ae2aa83bbcd8cd60b500c7c8f5578008
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-04-16 10:29:41 +0530

    changes

--- StreamlineAcademyWebApi/StreamlineAcademy.Domain/Models/Responses/InstructorResponseModel.cs
+++ StreamlineAcademyWebApi/StreamlineAcademy.Domain/Models/Responses/InstructorResponseModel.cs
@@ -21,6 +21,7 @@ namespace StreamlineAcademy.Domain.Models.Responses
         public string? CountryName { get; set; }
         public string? StateName { get; set; }
         public string? CityName { get; set; }
+        public string? AcademyName { get; set; }
         public UserRole UserRole { get; set; }
         public bool IsActive { get; set; }
     }
